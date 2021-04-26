61c9fe760e1af80a96ddfc67e0414f3067265bba
Cássio Miguel Entrudo <cassio.entrudo@gmail.com>
2021-04-26 17:57:14 -0300

    removido antigo "Ofertar Leitos"

--- hospital-dos-guri/FormGerenciamentoHospital.Designer.cs
+++ hospital-dos-guri/FormGerenciamentoHospital.Designer.cs
@@ -29,30 +29,27 @@ namespace hospital_dos_guri
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelAll = new System.Windows.Forms.Panel();
             this.gbOfertaLeitos = new System.Windows.Forms.GroupBox();
+            this.chbCancelarOferta = new System.Windows.Forms.CheckBox();
             this.lbNumeroLeitos = new System.Windows.Forms.Label();
             this.lbTipoLeito = new System.Windows.Forms.Label();
             this.btnOfertaLeito = new System.Windows.Forms.Button();
             this.nupNumeroLeitos = new System.Windows.Forms.NumericUpDown();
             this.cbTipoLeito = new System.Windows.Forms.ComboBox();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.gbSolicitacaoTransferencia = new System.Windows.Forms.GroupBox();
-            this.chbCancelarOferta = new System.Windows.Forms.CheckBox();
-            this.panel1.SuspendLayout();
+            this.panelAll.SuspendLayout();
             this.gbOfertaLeitos.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nupNumeroLeitos)).BeginInit();
-            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // panelAll
             // 
-            this.panel1.Controls.Add(this.gbOfertaLeitos);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(390, 301);
-            this.panel1.TabIndex = 0;
+            this.panelAll.Controls.Add(this.gbOfertaLeitos);
+            this.panelAll.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelAll.Location = new System.Drawing.Point(0, 0);
+            this.panelAll.Name = "panelAll";
+            this.panelAll.Size = new System.Drawing.Size(376, 274);
+            this.panelAll.TabIndex = 0;
             // 
             // gbOfertaLeitos
             // 
@@ -65,10 +62,21 @@ namespace hospital_dos_guri
             this.gbOfertaLeitos.Dock = System.Windows.Forms.DockStyle.Fill;
             this.gbOfertaLeitos.Location = new System.Drawing.Point(0, 0);
             this.gbOfertaLeitos.Name = "gbOfertaLeitos";
-            this.gbOfertaLeitos.Size = new System.Drawing.Size(390, 301);
+            this.gbOfertaLeitos.Size = new System.Drawing.Size(376, 274);
             this.gbOfertaLeitos.TabIndex = 0;
             this.gbOfertaLeitos.TabStop = false;
             this.gbOfertaLeitos.Text = "Ofertar/Cancelar Leitos";
+            // 
+            // chbCancelarOferta
+            // 
+            this.chbCancelarOferta.AutoSize = true;
+            this.chbCancelarOferta.Location = new System.Drawing.Point(7, 149);
+            this.chbCancelarOferta.Name = "chbCancelarOferta";
+            this.chbCancelarOferta.Size = new System.Drawing.Size(158, 19);
+            this.chbCancelarOferta.TabIndex = 5;
+            this.chbCancelarOferta.Text = "Cancelar Oferta de Leitos";
+            this.chbCancelarOferta.UseVisualStyleBackColor = true;
+            this.chbCancelarOferta.CheckedChanged += new System.EventHandler(this.chbCancelarOferta_CheckedChanged);
             // 
             // lbNumeroLeitos
             // 
@@ -92,9 +100,9 @@ namespace hospital_dos_guri
             // 
             this.btnOfertaLeito.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnOfertaLeito.Location = new System.Drawing.Point(6, 235);
+            this.btnOfertaLeito.Location = new System.Drawing.Point(6, 208);
             this.btnOfertaLeito.Name = "btnOfertaLeito";
-            this.btnOfertaLeito.Size = new System.Drawing.Size(361, 47);
+            this.btnOfertaLeito.Size = new System.Drawing.Size(347, 47);
             this.btnOfertaLeito.TabIndex = 2;
             this.btnOfertaLeito.Text = "Ofertar Leitos";
             this.btnOfertaLeito.UseVisualStyleBackColor = true;
@@ -129,67 +137,31 @@ namespace hospital_dos_guri
             this.cbTipoLeito.Size = new System.Drawing.Size(361, 23);
             this.cbTipoLeito.TabIndex = 0;
             // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.gbSolicitacaoTransferencia);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(390, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(400, 301);
-            this.panel2.TabIndex = 1;
-            // 
-            // gbSolicitacaoTransferencia
-            // 
-            this.gbSolicitacaoTransferencia.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.gbSolicitacaoTransferencia.Location = new System.Drawing.Point(6, 0);
-            this.gbSolicitacaoTransferencia.Name = "gbSolicitacaoTransferencia";
-            this.gbSolicitacaoTransferencia.Size = new System.Drawing.Size(394, 295);
-            this.gbSolicitacaoTransferencia.TabIndex = 0;
-            this.gbSolicitacaoTransferencia.TabStop = false;
-            this.gbSolicitacaoTransferencia.Text = "Solicitação de Transferência";
-            // 
-            // chbCancelarOferta
-            // 
-            this.chbCancelarOferta.AutoSize = true;
-            this.chbCancelarOferta.Location = new System.Drawing.Point(7, 149);
-            this.chbCancelarOferta.Name = "chbCancelarOferta";
-            this.chbCancelarOferta.Size = new System.Drawing.Size(158, 19);
-            this.chbCancelarOferta.TabIndex = 5;
-            this.chbCancelarOferta.Text = "Cancelar Oferta de Leitos";
-            this.chbCancelarOferta.UseVisualStyleBackColor = true;
-            this.chbCancelarOferta.CheckedChanged += new System.EventHandler(this.chbCancelarOferta_CheckedChanged);
-            // 
             // FormGerenciamentoHospital
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(790, 301);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(376, 274);
+            this.Controls.Add(this.panelAll);
             this.Name = "FormGerenciamentoHospital";
             this.Text = "Gerenciamento de Hospital";
-            this.panel1.ResumeLayout(false);
+            this.panelAll.ResumeLayout(false);
             this.gbOfertaLeitos.ResumeLayout(false);
             this.gbOfertaLeitos.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nupNumeroLeitos)).EndInit();
-            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panelAll;
         private System.Windows.Forms.GroupBox gbOfertaLeitos;
         private System.Windows.Forms.Label lbNumeroLeitos;
         private System.Windows.Forms.Label lbTipoLeito;
         private System.Windows.Forms.Button btnOfertaLeito;
         private System.Windows.Forms.NumericUpDown nupNumeroLeitos;
         private System.Windows.Forms.ComboBox cbTipoLeito;
-        private System.Windows.Forms.GroupBox gbSolicitacaoTransferencia;
         private System.Windows.Forms.CheckBox chbCancelarOferta;
     }
 }
